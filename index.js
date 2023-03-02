46380eaf5029846b851617864577d786481b6322
kenjung <j.pannawat45@gmail.com>
2023-03-02 16:18:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 // Base URL for the API
 // const base_url = "https://api.example.com";
-const base_url = "10.104.11.127:3000"; 
+const base_url = "http://10.104.11.127:3000"; 
 
 // Set the template engine
 app.set('view engine','ejs');
