5331fb0326bd8bb8e9b0b95fc6835bedb3718cf7
kenjung <j.pannawat45@gmail.com>
2023-03-02 15:46:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 // Base URL for the API
 // const base_url = "https://api.example.com";
-const base_url = "pannawat.proen.app.ruk-com.cloud"; 
+const base_url = "http://localhost:3000"; 
 
 // Set the template engine
 app.set('view engine','ejs');
