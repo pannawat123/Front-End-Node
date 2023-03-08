baee34ce3d400c3212bc3e46dfa86d7029ff2831
kenjung <69411868+pannawat123@users.noreply.github.com>
2023-03-08 09:46:06 +0700

    add

--- index.js
+++ index.js
@@ -3,19 +3,22 @@
 
 const express = require("express");
 const axios = require("axios");
-const app = express();
 var bodyParser = require("body-parser");
+const path = require("path")
+const app = express();
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
 // const base_url = "http://10.104.11.127:3000";
-// const base_url = "http://localhost:3000";
-const base_url = "http://node42135-pannawat.proen.app.ruk-com.cloud";
+const base_url = "http://localhost:3000";
+// const base_url = "http://node42135-pannawat.proen.app.ruk-com.cloud";
 
 // Set the template engine
+app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 
 // Serve static files
 app.use(express.static(__dirname + "/public"));
